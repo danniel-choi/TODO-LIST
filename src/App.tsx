c2fc9ff5e9d78654fd0911ae6ce2a920ba6e8e1d
DannielChoi <danniel.choi@gmail.com>
2021-09-24 18:08:08 +0900

    INPUT 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Styled from 'styled-components';
 
-import { Button } from 'Components';
+import { Button, Input } from 'Components';
 
 const Container = Styled.div`
   min-height: 100vh;
@@ -20,12 +20,17 @@ const Contents = Styled.div`
   box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
 `;
 
-
+const InputContainer = Styled.div`
+  display: flex;
+`;
 function App() {
   return (
     <Container>
       <Contents>
-        <Button label="추가" hoverColor="#000000" onClick={() => alert('추가')} />
+        <InputContainer>
+          <Input placeholder="할 일을 입력해 주세요" onChange={(text) => console.log(text)}/>
+          <Button label="추가" onClick={() => alert('추가')} />
+        </InputContainer>
       </Contents>
     </Container>
   );
