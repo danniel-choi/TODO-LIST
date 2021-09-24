979339682045bdf5b7d412a32634a2b8be8cb531
DannielChoi <danniel.choi@gmail.com>
2021-09-24 17:27:54 +0900

    버튼 컴포넌트 만들기

---
+++ src/Components/Button/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Styled from 'styled-components';
+
+const Container = Styled.div`
+    text-align: center;
+    background-color: #304FFE;
+    padding: 10px 20px;
+    border-radius: 8px;
+    cursor: pointer;
+    &: hover {
+        background-color: #1E40FF;
+    }
+    &: active {
+        box-shadow: inset 5px 5px 10px rgba(0, 0, 0, 0.2);
+    }
+`;
+
+const Label = Styled.div`
+    color: #FFFFFF;
+    font-size: 16px;
+`;
+
+interface Props {
+    readonly label: string;
+}
+
+export const Button = (props: Props) => {
+    return (
+        <Container>
+            <Label>{props.label}</Label>
+        </Container>
+    );
+}
+
